6765a18ef9b399334e772b0d990a184dd6c59d63
v76nl <218459059+v76nl@users.noreply.github.com>
2026-07-03 16:06:30 +0900

    chore(main): デバッグ用DOMエラー表示コードを削除
    デバッグ目的で一時的に挿入したwindow.onerror / console.error上書きのコードを除去。

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,3 @@
-window.addEventListener('error', e => document.body.insertAdjacentHTML('beforeend', '<div style="position:fixed;top:0;left:0;z-index:9999;background:red;color:white;">ERROR: '+e.message+'</div>'));
-window.addEventListener('unhandledrejection', e => document.body.insertAdjacentHTML('beforeend', '<div style="position:fixed;top:0;left:0;z-index:9999;background:red;color:white;">REJECTION: '+e.reason+'</div>'));
-const oldError = console.error;
-console.error = (...args) => {
-    document.body.insertAdjacentHTML('beforeend', '<div style="position:fixed;top:50px;left:0;z-index:9999;background:orange;color:white;">CONSOLE.ERROR: '+args.join(' ')+'</div>');
-    oldError(...args);
-};
 import { loadFont } from './geometry.ts';
 import { initUIFromState, initEvents } from './ui.ts';
 import { animate } from './scene.ts';
